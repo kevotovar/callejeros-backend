5a531a59f2c368bd57531ad1699cba88e78f013c
Sergio Ramos <sergioramos@MacBook-Pro-de-Katia.local>
2017-03-03 13:22:38 -0600

    cambios

--- callejeritos/settings.py
+++ callejeritos/settings.py
@@ -43,12 +43,14 @@ INSTALLED_APPS = [
     'modules.solicitudes_adopcion',
     'rest_framework',
     'rest_framework_swagger',
-    'django_filters'
+    'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -106,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
