96fd4d889405be4ecaa202792cca85d4756d8daf
Sergio Ramos <sergioramos@MacBook-Pro-de-Katia.local>
2017-03-06 13:50:56 -0600

    changes

--- modules/datos_user/views.py
+++ modules/datos_user/views.py
@@ -5,10 +5,10 @@ from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
 import django_filters.rest_framework
 # Create your views here.
 class UserList(generics.ListCreateAPIView):
 	queryset = User.objects.all()
 	serializer_class = UserSerializer
-	permission_classes = (AllowAny,)
+	permission_classes = (IsAuthenticated,)
